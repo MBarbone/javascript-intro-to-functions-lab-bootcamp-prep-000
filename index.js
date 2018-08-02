260f6f8e4a32454970a96ba0b53d555a6ab74204
Michael Barbone <michael.barbone@gmail.com>
2018-08-02 01:46:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string.toUpperCase) {
-  console.log(string)
-}
+function logString
 
+
+
+
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+}
+else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+} else if ('I love you, Grandma.') {
+  return 'I love you, too.'
+}
